e5a55f9929433ddee79ba6166b37b37c84a38916
Zac Kitcher <ZKitcher@HamiltonMFG.com>
2024-07-12 12:15:54 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,83 @@
+# State Management with React Context and Hooks
+
+This project demonstrates a simple yet effective state management setup using React Context API and custom hooks. It provides a way to manage global application state while persisting it in local storage.
+
+## Features
+
+- **Global Context**: Utilizes React Context to manage global state across components.
+- **Reducer**: Implements a reducer function to handle state transitions based on dispatched actions.
+- **Persistence**: Uses local storage to persist state between sessions.
+- **Enum Class**: Provides an enumeration class for managing action types in a structured way.
+- **Custom Hooks**: Includes custom hooks (readKwestJournal and usePersistedState) for easy state management and persistence.
+
+## Installation
+
+1. Clone the repository:
+
+```bash
+npm i kwest-journal
+```
+
+## Usage
+1. Import the StateProvider component and wrap it around your application's root component in index.js or App.js:
+
+```javascript
+import StateProvider from './path/to/StateProvider';
+
+function App() {
+    return (
+        <StateProvider>
+            <YourApp />
+        </StateProvider>
+    );
+}
+```
+
+2. Access global state and dispatch actions using the readKwestJournal hook within your components:
+
+```javascript
+import { readKwestJournal } from './path/to/StateProvider';
+
+function YourComponent() {
+    const { state: { version }, helloWorld } = readKwestJournal();
+
+    return (
+        <div>
+            <p>Current version: {version}</p>
+            <button onClick={helloWorld}>Say Hello World!</button>
+        </div>
+    );
+}
+```
+
+3. Customize the Reducer function to add more actions and manage state transitions according to your application's needs.
+
+## API Reference
+
+### StateProvider
+
+- Props:
+  - children: React component(s) to be wrapped by the provider.
+  - localKey: Key used for local storage persistence.
+  - initialValues: Initial state values.
+  - reducer: Reducer function for handling state transitions.
+  - clearState: Boolean to have the state cleared each init.
+
+### readKwestJournal
+
+- Returns:
+  - state: Current global state.
+  - dispatch: Function to dispatch actions.
+  - Custom functions for interacting with global state (e.g., helloWorld).
+
+### clearAllPersistedState
+
+- Clears all persisted state related to Kwest-Journal from local storage.
+
+### clearCache
+
+- Clears all caches.
+
+## License
+
+This project is licensed under the MIT License - see the LICENSE.md file for details.
---
+++ index.js
@@ -0,0 +1,123 @@
+import React, { createContext, useReducer, useContext, useEffect, useState, } from 'react';
+
+export class Enum {
+    constructor(listOfKeys, startingIndex = 0) {
+        this.keys = [...listOfKeys];
+
+        listOfKeys.forEach(e => {
+            this[e] = startingIndex;
+            this[startingIndex] = e;
+            startingIndex++;
+        });
+
+        Object.freeze(this);
+    }
+
+    getKey(value) {
+        const key = this[value];
+        if (key === undefined) {
+            throw new Error(`Invalid enum value: ${value}`);
+        }
+        return key;
+    }
+}
+
+const initialState = {
+    version: '1.0.0'
+}
+
+export const GlobalContext = createContext(initialState);
+
+const StateProvider = ({ children, localKey = 'global', initialValues = initialState, reducer = Reducer, clearState = false }) => {
+    if (clearState) clearAllPersistedState();
+
+    const [persistedState, setPersistedState] = usePersistedState(localKey, initialValues);
+    const [state, dispatch] = useReducer(reducer, persistedState);
+
+    useEffect(() => {
+        setPersistedState(state);
+    }, [setPersistedState, state]);
+
+    return (
+        <GlobalContext.Provider value={{ state, dispatch }}>
+            {children}
+        </GlobalContext.Provider>
+    );
+}
+
+const ActionType = new Enum([
+    'HELLO_WORLD',
+]);
+
+export function readKwestJournal() {
+    const { state, dispatch } = useContext(GlobalContext);
+
+    const [functions] = useState({
+        helloWorld: () => {
+            dispatch({ type: ActionType.HELLO_WORLD, payload: 'Hello World!' });
+        },
+    });
+
+    return {
+        ...functions,
+        state,
+    };
+}
+
+const Reducer = (state = initialState, action) => {
+    let newState = structuredClone(state);
+    switch (action.type) {
+        case ActionType.HELLO_WORLD:
+            alert(action.payload)
+            break;
+
+        default:
+            console.error(`Unknown action type: ${action.type}`);
+            throw new Error();
+    }
+    return newState;
+};
+
+const usePersistedState = (key, initialState) => {
+    const storageKey = `Kwest-Journal.${key}`;
+    const hydrateState = () => {
+        try {
+            const persisted = window.localStorage.getItem(storageKey);
+            const parsedRes = JSON.parse(persisted);
+
+            if (parsedRes.version !== initialState.version) {
+                console.warn('State version mismatch, returning initial state...')
+                return initialState;
+            }
+            return parsedRes || initialState;
+        } catch {
+            console.warn('Failed to read current state version, returning initial state...')
+            return initialState;
+        }
+    };
+
+    const [state, setState] = useState(hydrateState);
+
+    useEffect(() => {
+        window.localStorage.setItem(storageKey, JSON.stringify(state));
+    }, [state, storageKey]);
+
+    return [state, setState];
+}
+
+export const clearAllPersistedState = () => {
+    console.log('Clearing State')
+    Object.keys(window.localStorage).forEach(key => {
+        if (key.startsWith('Kwest-Journal.')) {
+            window.localStorage.removeItem(key);
+        }
+    });
+}
+
+export const clearCache = () => {
+    caches
+        .keys()
+        .then((names) => names.forEach(e => caches.delete(e)));
+}
+
+export default StateProvider;
